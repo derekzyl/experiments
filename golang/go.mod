8835dafb206da60e21409e26e334718019ba8bba
derek og <derekzyl@gmail.com>
2023-08-30 22:14:10 +0100

    go lang revisit

---
+++ golang/go.mod
@@ -0,0 +1,3 @@
+module github.com/derekzyl
+
+go 1.20
---
+++ golang/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		hello(i)
+	}
+
+	first := 100
+	second := &first
+	third := &second
+	fourth := &third
+
+	fmt.Println(first)
+	fmt.Println(*second)
+	fmt.Println(**third)
+	fmt.Println(***fourth)
+
+	names := []string{"first", "second", "third", "fourth"}
+	n := make([]string, len(names))
+
+	n = names
+	sort.Strings(names[:])
+
+}
+
+func hello(num int) {
+	fmt.Println(num)
+}
+
+//create a function that takes a number and increases it by one
